63355a3cb8c62511ff5b21160a8421ecccc60d82
ushoa <55466500+ushoa@users.noreply.github.com>
2019-09-20 15:14:23 -0300

    Add files via upload

---
+++ js/jsFormulario.js
@@ -0,0 +1,15 @@
+function inc (){
+    document.getElementById('inc').style.display ="block";
+    document.getElementById('cacc').style.display ="none";
+    document.getElementById('ppp').style.display ="none";
+}
+function cacc (){
+    document.getElementById('inc').style.display ="none";
+    document.getElementById('cacc').style.display ="block";
+    document.getElementById('ppp').style.display ="none";
+}
+function ppp(){
+    document.getElementById('inc').style.display ="none";
+    document.getElementById('cacc').style.display ="none";
+    document.getElementById('ppp').style.display ="block";
+}
